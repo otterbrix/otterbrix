e73c4552a4899c01103030d57048b7518ba44aea
OrangeSmokingJacket <whereisgeol@gmail.com>
2025-10-15 17:48:03 +0200

    wip

--- integration/python/otterbrix/get_path.py
+++ integration/python/otterbrix/get_path.py
@@ -1,6 +1,6 @@
 import skbuild.constants
 
-build_dir = skbuild.constants.CMAKE_BUILD_DIR() + "/build/Release/generators"
+build_dir = skbuild.constants.CMAKE_BUILD_DIR()
 
 if __name__ == '__main__':  # pragma: no cover
     print(build_dir)

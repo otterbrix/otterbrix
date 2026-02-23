e6ecd4f43282a7619f120182d1af11af6b150f71
kotbegemot <k0tb9g9m0t@gmail.com>
2026-02-23 19:33:02 +0400

    split simple_predicate v2

--- components/physical_plan/operators/predicates/simple_predicate.cpp
+++ components/physical_plan/operators/predicates/simple_predicate.cpp
@@ -1,6 +1,59 @@
-#include "simple_predicate_impl.hpp"
+#include "simple_predicate.hpp"
+#include "utils.hpp"
+
+#include <regex>
 
 namespace components::operators::predicates {
+
+    namespace {
+
+        // Because regex is not a constexpr(?), we use it to dispatch function
+        template<typename T = void>
+        struct regex;
+
+        template<>
+        struct regex<void> {};
+
+        template<typename COMP, typename T, typename U>
+        bool evaluate_comp(T left, U right) requires(!std::is_same_v<COMP, regex<>>) {
+            return COMP{}(left, right);
+        }
+
+        template<typename COMP, typename T, typename U>
+        bool evaluate_comp(T, U) requires(std::is_same_v<COMP, regex<>>) {
+            throw std::runtime_error("incorrect argument type for regex");
+        }
+
+        template<typename COMP>
+        bool evaluate_comp(std::string_view left, std::string_view right) requires(std::is_same_v<COMP, regex<>>) {
+            return std::regex_search(std::string(left), std::regex(std::string(right)));
+        }
+
+        template<typename COMP>
+        bool evaluate_comp(const types::logical_value_t& left,
+                           const types::logical_value_t& right) requires(std::is_same_v<COMP, regex<>>) {
+            return evaluate_comp<COMP>(left.value<std::string_view>(), right.value<std::string_view>());
+        }
+
+        template<typename COMP>
+        simple_predicate::check_function_t
+        make_comparator(std::pmr::memory_resource* resource,
+                        const compute::function_registry_t* function_registry,
+                        const expressions::compare_expression_ptr& expr,
+                        const logical_plan::storage_parameters* parameters) {
+            auto left_getter = impl::create_value_getter(resource, function_registry, expr->left(), parameters);
+            auto right_getter = impl::create_value_getter(resource, function_registry, expr->right(), parameters);
+            return [left_getter = std::move(left_getter),
+                    right_getter = std::move(right_getter)](const vector::data_chunk_t& chunk_left,
+                                                            const vector::data_chunk_t& chunk_right,
+                                                            size_t index_left,
+                                                            size_t index_right) {
+                return evaluate_comp<COMP>(left_getter(chunk_left, chunk_right, index_left, index_right),
+                                           right_getter(chunk_left, chunk_right, index_left, index_right));
+            };
+        }
+
+    } // anonymous namespace
 
     simple_predicate::simple_predicate(check_function_t func)
         : func_(std::move(func)) {}
@@ -62,26 +115,25 @@ namespace components::operators::predicates {
             }
             case compare_type::eq:
                 return {new simple_predicate(
-                    create_eq_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
+                    make_comparator<std::equal_to<>>(resource, function_registry, expr, parameters))};
             case compare_type::ne:
                 return {new simple_predicate(
-                    create_ne_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
+                    make_comparator<std::not_equal_to<>>(resource, function_registry, expr, parameters))};
             case compare_type::gt:
                 return {new simple_predicate(
-                    create_gt_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
+                    make_comparator<std::greater<>>(resource, function_registry, expr, parameters))};
             case compare_type::gte:
                 return {new simple_predicate(
-                    create_gte_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
+                    make_comparator<std::greater_equal<>>(resource, function_registry, expr, parameters))};
             case compare_type::lt:
                 return {new simple_predicate(
-                    create_lt_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
+                    make_comparator<std::less<>>(resource, function_registry, expr, parameters))};
             case compare_type::lte:
                 return {new simple_predicate(
-                    create_lte_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
-            case compare_type::regex: {
+                    make_comparator<std::less_equal<>>(resource, function_registry, expr, parameters))};
+            case compare_type::regex:
                 return {new simple_predicate(
-                    create_regex_simple_predicate(resource, function_registry, expr, types_left, types_right, parameters))};
-            }
+                    make_comparator<regex<>>(resource, function_registry, expr, parameters))};
             case compare_type::all_false:
                 return {new simple_predicate(
                     [](const vector::data_chunk_t&, const vector::data_chunk_t&, size_t, size_t) { return false; })};

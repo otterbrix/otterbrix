e88d14951bc6018b796b968721c34500802cb4b0
kotbegemot <k0tb9g9m0t@gmail.com>
2026-02-27 07:55:26 +0200

    fix

--- components/vector/arithmetic.hpp
+++ components/vector/arithmetic.hpp
@@ -6,6 +6,21 @@
 
 namespace components::vector {
 
+    namespace detail {
+        template<typename R>
+        constexpr bool is_zero(R b) {
+            if constexpr (std::is_floating_point_v<R>) {
+                return !(b > R{0} || b < R{0});
+            } else {
+                return b == R{0};
+            }
+        }
+
+        // Common result type that avoids sign-promotion issues with int128
+        template<typename L, typename R>
+        using safe_result_t = decltype(static_cast<L>(0) + static_cast<R>(0));
+    } // namespace detail
+
     // Safe divides that returns 0 on division by zero
     template<typename T = void>
     struct safe_divides;
@@ -13,11 +28,11 @@ namespace components::vector {
     struct safe_divides<void> {
         template<typename L, typename R>
         constexpr auto operator()(L a, R b) const {
-            using result_t = decltype(a + b);
-            if (b == R{0}) {
-                return result_t{0};
+            using result_t = detail::safe_result_t<L, R>;
+            if (detail::is_zero(b)) {
+                return static_cast<result_t>(0);
             }
-            return static_cast<result_t>(a) / static_cast<result_t>(b);
+            return static_cast<result_t>(static_cast<result_t>(a) / static_cast<result_t>(b));
         }
     };
 
@@ -28,14 +43,14 @@ namespace components::vector {
     struct safe_modulus<void> {
         template<typename L, typename R>
         constexpr auto operator()(L a, R b) const {
-            using result_t = decltype(a + b);
-            if (b == R{0}) {
-                return result_t{0};
+            using result_t = detail::safe_result_t<L, R>;
+            if (detail::is_zero(b)) {
+                return static_cast<result_t>(0);
             }
             if constexpr (std::is_floating_point_v<result_t>) {
-                return std::fmod(static_cast<result_t>(a), static_cast<result_t>(b));
+                return static_cast<result_t>(std::fmod(static_cast<result_t>(a), static_cast<result_t>(b)));
             } else {
-                return static_cast<result_t>(a) % static_cast<result_t>(b);
+                return static_cast<result_t>(static_cast<result_t>(a) % static_cast<result_t>(b));
             }
         }
     };

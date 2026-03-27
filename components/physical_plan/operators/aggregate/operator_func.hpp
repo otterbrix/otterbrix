6f25a8810185804af3e7d5db35028d6efc5baca9
Mikhail Fedorenko <fvmikhail@gmail.com>
2026-03-27 10:07:05 +0300

    UDF vectorization in physical plan

--- components/physical_plan/operators/aggregate/operator_func.hpp
+++ components/physical_plan/operators/aggregate/operator_func.hpp
@@ -19,6 +19,7 @@ namespace components::operators::aggregate {
 
         const compute::function* func() const { return func_; }
         const std::pmr::vector<expressions::param_storage>& args() const { return args_; }
+        bool distinct() const { return distinct_; }
 
     private:
         std::pmr::vector<expressions::param_storage> args_;
@@ -26,6 +27,7 @@ namespace components::operators::aggregate {
         bool distinct_{false};
 
         types::logical_value_t aggregate_impl(pipeline::context_t* pipeline_context) override;
+        compute::datum_t aggregate_batch_impl(pipeline::context_t* pipeline_context) override;
         std::string key_impl() const override;
     };
 
